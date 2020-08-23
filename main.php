300278a725d3a6953aaa158221366688ac9076ce
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-08-23 01:15:01 -0300

    query delete and insert

--- main.php
+++ main.php
@@ -1,35 +1,35 @@
 <?php
 
-if(isset($_GET['search-name'])) {
-    //database connection
-    $server = 'localhost';
-    $usuario = 'manager';
-    $password = 'manager123456';
-    $database = 'students';
+//database connection
+$server = 'localhost';
+$usuario = 'manager';
+$password = 'manager123456';
+$database = 'students';
 
 $conn = new mysqli($server, $usuario, $password, $database) or die("connection error!");
 
+if (isset($_GET['search-name'])) {
 
-//search in database
-$name = $_GET['search-name'];
+    //search in database
+    $myname = $_GET['search-name'];
 
-if(empty($name)) {
-    $searchQuery = "SELECT * FROM student_data";
-} else {
-    $searchQuery = "SELECT * FROM student_data WHERE name LIKE '%$name%'";
-}
+    if (empty($myname)) {
+        $searchQuery = "SELECT * FROM student_data";
+    } else {
+        $searchQuery = "SELECT * FROM student_data WHERE nome LIKE '%$myname%'";
+    }
 
-$sql = $conn->query($searchQuery);
+    $sql = $conn->query($searchQuery);
 
-$rowCount = $sql->num_rows;
+    $rowCount = $sql->num_rows;
 
-sleep(1.5);
+    sleep(1.5);
 
-if($rowCount > 0) {
+    if ($rowCount > 0) {
 
-//view
+        //view
 
-echo"
+        echo "
     <h3 class='display-8'>Results</h3>
     <hr>
    <table class='table table-bordered table-hover'>
@@ -40,25 +40,85 @@ echo"
                 <th>E-mail</th>
                 <th>Phone Number</th>
                 <th>Address</th>
+                <th>Actions</th>
             </tr>
         </thead>
-        <tbody>"; 
-while($row = $sql->fetch_array()){
-        echo '<tr>';
-            echo '<td>'.$row['id'].'</td>';
-            echo '<td>'.$row['name'].'</td>';
-            echo '<td>'.$row['email'].'</td>';
-            echo '<td>'.$row['phone_contact'].'</td>';
-            echo '<td>'.$row['address'].'</td>';
-        echo '</tr>';
-    }
+        <tbody>";
+        while ($row = $sql->fetch_array()) {
+            echo '<tr>';
+            echo '<td>' . $row['id'] . '</td>';
+            echo '<td>' . $row['nome'] . '</td>';
+            echo '<td>' . $row['email'] . '</td>';
+            echo '<td>' . $row['phone_contact'] . '</td>';
+            echo '<td>' . $row['address_inf'] . '</td>';
+            echo '<td>' . '<a href="#" type="button" class="btn btn-danger" onclick="deleteStudent(' . $row['id'] . ')"><i class="fas fa-trash-alt"></i></a>'. '</td>';
+            echo '</tr>';
+        }
 
-    echo '</tbody>
+        echo '</tbody>
     </table>';
-} else { //end of if($rowCount)
-    echo "
+    } else { //end of if($rowCount)
+        echo "
     <h4 class='display-8'>No record found!</h4>
     ";
+    }
+} elseif (
+
+    isset($_GET['name']) and
+    isset($_GET['address']) and
+    isset($_GET['phone_contact']) and
+    isset($_GET['email'])
+
+) {
+    $myname = $_GET['name'];
+    $myaddress = $_GET['address'];
+    $phone_contact = $_GET['phone_contact'];
+    $email = $_GET['email'];
+
+    $query = "INSERT INTO student_data(nome, email , phone_contact, address_inf) VALUES ('$myname', '$email', '$phone_contact', '$myaddress')";
+    
+    sleep(1);
+
+    $sql = $conn->query($query);
+
+   echo '
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>The registration was successful!</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+';
+    
+} elseif(isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+
+    $query = "DELETE FROM student_data WHERE ID = $id";
+    
+    sleep(1);
+
+    $sql = $conn->query($query);
+
+   echo '
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Record deleted with success!</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close" onclick="searchData()">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+';
+
 }
 
+
+else {
+    
+    echo '
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>An error occurred while registering the user. Try again!</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+';
 }
--- scriptsjs/ajax.js
+++ scriptsjs/ajax.js
@@ -7,9 +7,9 @@ function searchData() {
 
     ajax.open('GET', `main.php?search-name=${name}`, true)
 
-    ajax.onreadystatechange = function() {
-        if(ajax.readyState == 4){
-            if(ajax.status == 200) {
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4) {
+            if (ajax.status == 200) {
                 result.innerHTML = ajax.responseText
             } else {
                 result.innerHTML = `There was a connection error: ${ajax.statusText}!`
@@ -19,4 +19,57 @@ function searchData() {
 
     ajax.send(null)
 
+}
+
+function insertData() {
+
+    let name = document.getElementById('insertName').value
+    let address = document.getElementById('insertAddress').value
+    let phone_contact = document.getElementById('insertPhone').value
+    let email = document.getElementById('insertEmail').value
+
+    console.log(name, address, phone_contact, email)
+
+    let response = document.getElementById('res')
+
+    let ajax = new XMLHttpRequest()
+
+    response.innerHTML = '<img src="img/loading_icon.jpg" width="120px">'
+
+    ajax.open("GET", `main.php?name=${name}&address=${address}&phone_contact=${phone_contact}&email=${email}`, true)
+
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4) {
+            if (ajax.status == 200) {
+                response.innerHTML = ajax.responseText
+            } else {
+                response.innerHTML = `There was a connection error: ${ajax.statusText}!`
+            }
+        }
+    }
+
+    ajax.send(null)
+
+}
+
+function deleteStudent(id) {
+    let result = document.getElementById('jumbotron-person')
+    let ajax = new XMLHttpRequest()
+
+    result.innerHTML = '<img src="img/loading_icon.jpg" width="120px">'
+
+
+    ajax.open('GET', `main.php?delete=${id}`, true)
+
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4) {
+            if (ajax.status == 200) {
+                result.innerHTML = ajax.responseText
+            } else {
+                result.innerHTML = `There was a connection error: ${ajax.statusText}!`
+            }
+        }
+    }
+
+    ajax.send(null)
 }
